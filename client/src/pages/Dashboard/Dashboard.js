f515e9c947c9fb8f1fbf92da9ed91e97a164e0c4
Patty C <patrickcorcoran10@gmail.com>
2019-04-02 18:16:15 -0500

    Opps and Dashboard pages have basic styling and component installs

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Graph from '../../components/Graph/Graph';
+import '../../pages/Dashboard/Dashboard.css';
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <p>Dashboard</p>
+      <div className='container'>
+        <div className='row'>
+            <div className='col-md-4'>
+            </div>
+            <div className='col-md-4'>
+                <p>Dashboard</p>  
+            </div>
+            <div className='col-md-4'>
+            </div>
+        </div>
+        <div className='row'>
+            <div className='col-md-1'>
+
+            </div>
+            <div className='col-md-3'>
+                <div className='box'>
+                    <h6>New Opportunities Created</h6>
+                    <h6>##</h6>
+                </div>
+            </div>
+            <div className='col-md-1'>
+                
+            </div>
+            <div className='col-md-3'>
+                <div className='box'>
+                    <h6>Potential Monthly Revenue</h6>
+                    <h6>##</h6>
+                </div> 
+            </div>
+            <div className='col-md-1'>
+                
+            </div>
+            <div className='col-md-3'>
+                <div className='box'>
+                    <h6>Opportunities Started/Completed</h6>
+                    <h6>##</h6>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div className='row'>
+            <div className='col-md-2'>
+            </div>
+            <div className='col-md-8'>
+                <Graph/>
+            </div>
+            <div className='col-md-2'>
+            </div>
+        </div>
       </div>
+      
     )
   }
 }
