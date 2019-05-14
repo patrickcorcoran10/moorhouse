b28379b0b3ad06bb8c0bf0ee162ba1c26d2f4348
Patty C <patrickcorcoran10@gmail.com>
2019-05-14 14:04:03 -0500

    jawsdb

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 //     })
 // };
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.JAWSDB_URL === 'production') {
     app.use(express.static('client/build'));
 };
 
